5052da4ab98a7dbee040d9b9b87db2362d0a38c7
meher92 <meher92@users.noreply.github.com>
2017-05-31 23:12:07 +0530

    added linear reg and word2vec tf code

--- cs224n/word2vec/word2vec.py
+++ cs224n/word2vec/word2vec.py
@@ -19,7 +19,7 @@ Consider the following sentence:
 With a window size of 1, we have the dataset:
 ([the, cs224n], first), ([lot, fun], of) ...
 
-Remember that Skipgram tries to predict each context word from 
+Remember that Skipgram tries to predict each context word from
 its target word, and so the task becomes to predict 'the' and
 'cs224n' from first, 'lot' and 'fun' from 'of' and so on.
 
@@ -45,14 +45,64 @@ print("Number of training examples:", len(train_data)*batch_size)
 print("Number of validation examples:", len(val_data))
 
 def skipgram():
-    raise NotImplementedError
+    batch_inputs = tf.placeholder(tf.int32, shape=[batch_size,])
+    batch_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+    val_dataset = tf.constant(val_data, dtype=tf.int32)
+
+    with tf.variable_scope("word2vec") as scope:
+        embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+
+        batch_embeddings = tf.nn.embedding_lookup(embeddings, batch_inputs)
+        weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size], stddev = 1.0/math.sqrt(embedding_size)))
+        biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+        loss = tf.reduce_mean(tf.nn.nce_loss(weights=weights,
+                                             biases=biases,
+                                             labels=batch_labels,
+                                             inputs=batch_embeddings,
+                                             num_sampled=num_sampled,
+                                             num_classes=vocabulary_size))
+
+        norm = tf.sqrt(tf.reduce_mean(tf.square(embeddings), 1, keep_dims=True))
+        normalized_embeddings = embeddings/norm
+
+        val_embeddings = tf.nn.embedding_lookup(normalized_embeddings, val_dataset)
+        similarity = tf.matmul(val_embeddings, normalized_embeddings, transpose_b=True)
+        return batch_inputs, batch_labels, normalized_embeddings, loss, similarity
+
 
 def run():
-    raise NotImplementedError
+    batch_inputs, batch_labels, normalized_embeddings, loss, similarity = skipgram()
+    optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+
+        average_loss = 0.0
+        for step, batch_data in enumerate(train_data):
+            inputs, labels = batch_data
+            feed_dict = {batch_inputs: inputs, batch_labels: labels}
+
+            _, loss_val = sess.run([optimizer, loss], feed_dict)
+            average_loss += loss_val
+
+            if step % 1000 == 0:
+                if step > 0:
+                    average_loss /= 1000
+                    print("loss at iter", step, ":", average_loss)
+                    average_loss = 0
+            if step % 5000 == 0:
+                sim = similarity.eval()
+                for i in xrange(len(val_data)):
+                    top_k = 8
+                    nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                    print_closest_words(val_data[i], nearest, reverse_dictionary)
+
+        final_embeddings = normalized_embeddings.eval()
 
 # Let's start training
 final_embeddings = run()
 
 # Visualize the embeddings.
 visualize_embeddings(final_embeddings, reverse_dictionary)
-
